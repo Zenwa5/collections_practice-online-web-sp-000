1f177460a4b88252de65ba08181923ac857b0379
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-19 15:25:27 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
+n = []
+def sort_array_asc(n)
+  n.sort
+end
 
+def sort_array_desc(n)
+  n.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(n)
+  n.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array, 1, 2)
+end
+ def swap_elements_from_to(array, index, destination_index)
+  swapped_array = array
+  original = array[index]
+  swapped_array[index] = array[destination_index]
+  swapped_array[destination_index] = original
+  swapped_array
+end
